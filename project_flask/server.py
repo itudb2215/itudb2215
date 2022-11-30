5fbf470f14bffde69dad0ce37cd98af0d4f6e73c
Ayca Gökdağ <gokdag19@itu.edu.tr>
2022-11-30 19:54:08 +0300

    Database connection added

--- project_flask/server.py
+++ project_flask/server.py
@@ -1,10 +1,17 @@
+import os
+import psycopg2
 from datetime import datetime
-
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='flask_db',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
 
 @app.route("/")
 def home_page():
