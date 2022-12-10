5198e89d9c75a4e52cce8707b9f1172fe3ebe98d
Ayca Gökdağ <gokdag19@itu.edu.tr>
2022-12-10 19:21:22 +0300

    Refine database connection codes

--- project_flask/database.py
+++ project_flask/database.py
@@ -8,17 +8,27 @@ class Database:
     def get_game(self, game_id):
         with psycopg2.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            query = "SELECT TITLE, YR FROM GAME WHERE (ID = 10)"
+            query = "SELECT TITLE, YR FROM GAME WHERE (ID = ?)"
             cursor.execute(query, (game_id))
             title, year = cursor.fetchone()
         game_ = Game(title, year=year)
         return game_
 
     def get_games(self):
-        movies = []
+        games = []
         with psycopg2.connect(self.dbfile) as connection:
             cursor = connection.cursor()
             query = "SELECT ID, TITLE, YR FROM GAME ORDER BY ID"
+            cursor.execute(query)
+            for game_id, title, year in cursor:
+                games.append((game_id, Game(title, year)))
+        return games
+                
+    def get_comments(self):
+        movies = []
+        with psycopg2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT COMMENT, TITLE, YR FROM GAME ORDER BY ID"
             cursor.execute(query)
             for game_id, title, year in cursor:
                 movies.append((game_id, Game(title, year)))
--- project_flask/views.py
+++ project_flask/views.py
@@ -1,16 +1,21 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, current_app, render_template
 
 app = Flask(__name__)
 
 def home_page():
+    db = current_app.config["db"]
+    games = db.get_games()
     today = datetime.today()
     day_name = today.strftime("%A")
-    return render_template("home.html", day=day_name)
+    return render_template("home.html",day=day_name, movies=sorted(games))
 
 def games_page():
-    return render_template("games.html")
+    db = current_app.config["db"]
+    games = db.get_game(game_id)
+    today = datetime.today()
+    return render_template("games.html", movie_key=sorted(games))
 
 def price_info_page():
     return render_template("price_info.html")
