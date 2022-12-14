564aa486a851b3ccf1c66b44f007c11323f8beea
Ayca Gökdağ <gokdag19@itu.edu.tr>
2022-12-14 23:32:32 +0300

    Modify games.py

--- project_flask/games.py
+++ project_flask/games.py
@@ -1,11 +1,13 @@
 class Game:
-    def __init__(self, _game_id, _query_name, _release_year=None, _required_age=None, _metacritic=None, _about_text=None, _image_url=None,
+    def __init__(self, _game_id, _response_id, _query_name, _release_year=None, _required_age=None, _metacritic=None, _about_text=None, _image_url=None,
                  ):
         self.game_id = _game_id
+        self.response_id = _response_id
+        self.query_name = _query_name
         self.release_year = _release_year
         self.required_age = _required_age
-        self.query_name = _query_name
         self.metacritic = _metacritic
         self.about_text = _about_text
+        
         self.image_url = _image_url
         
