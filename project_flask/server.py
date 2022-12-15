9382d1766dd38eea4fd050e7df9a521a0732f1f1
Aybike Zeynep Taşkın <71091930+aybikezeyneptaskin@users.noreply.github.com>
2022-12-15 15:14:32 +0300

    Add genre page

--- project_flask/server.py
+++ project_flask/server.py
@@ -15,6 +15,7 @@ def create_app(debug=True):
     app.add_url_rule("/price_info/<game_id>", view_func=views.price_info_page)
     app.add_url_rule("/games/requirements/<game_id>", view_func=views.requirements_page)
     app.add_url_rule("/games/user_info", view_func=views.user_info_page)
+    app.add_url_rule("/games/genre/<game_id>", view_func=views.genre_page)
     
     
     return app
--- project_flask/views.py
+++ project_flask/views.py
@@ -11,12 +11,24 @@ def home_page():
     day_name = today.strftime("%A")
     return render_template("home.html",day=day_name, games=games)
 
+def delete_page():
+    db = Database(get_db())
+    games = db.get_games()
+    return render_template("delete_additionalinfo.html", games=games)
+
+def genre_page(game_id):
+    db = Database(get_db())
+    game = db.get_game(game_id)
+    genre = db.get_genre(game_id)
+    return render_template("genre.html", selected_game=game, genre=genre)
+
 def games_page(game_id):
     db = Database(get_db())
     game = db.get_game(game_id)
     additional = db.get_additional()
     adds = db.get_adds(game_id)
-    return render_template("games.html", selected_game=game, additional=additional, adds=adds)
+    genre = db.get_genre(game_id)
+    return render_template("games.html", selected_game=game, additional=additional, adds=adds, genre=genre)
 
 def price_info_page(game_id):
     db = Database(get_db())
