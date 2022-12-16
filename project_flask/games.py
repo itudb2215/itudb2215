4760e7c0844b0248d7077d88c918e1a8a8b99988
flyence <linayence@gmail.com>
2022-12-16 23:25:08 +0300

    ADD price_info class and game_tags class

--- project_flask/games.py
+++ project_flask/games.py
@@ -1,3 +1,6 @@
+from turtle import filling
+
+
 class Game:
     def __init__(self, _game_id, _query_name, _release_year=None, _required_age=None, _metacritic=None, _about_text=None, _image_url=None,
                  ):
@@ -78,4 +81,51 @@ class Author:
         self.playtime_forever = _playtime_forever
         self.playtime_last_two_weeks = _playtime_last_two_weeks
         self.last_played = _last_played
+
+
+class Price_Info:
+    def __init__(self, _price_id, _game_id=None, _isFree=None, _freeveravail=None, _pricecurrency=None, _priceinitial=None, _pricefinal=None, _purchassesavail=None, _subscriptionavail=None,
+                 ):
+                 
+        self.price_id = _price_id
+        self.game_id = _game_id
+        self.isFree = _isFree
+        self.freeveravail = _freeveravail
+        self.pricecurrency = _pricecurrency
+        self.priceinitial = _priceinitial
+        self.pricefinal = _pricefinal
+        self.purchaseavail = _purchassesavail
+        self.subscriptionavail = _subscriptionavail
         
+
+class Game_Tags:
+    def __init__(self, _tags_Id, _game_id=None, _addictive=None, _adventure=None, _co_op=None, _comedy=None, _crime=None, _drama=None, _dystopian=None, _education=None, _emotional=None, _epic=None, _family_friendly=None, _farming=None, _fighting=None, _flight=None, _football=None, _funny=None, _gambling=None, _hacking=None, _horror=None, _indie=None, _magic=None, _mythology=None, _platformer=None, _rpg=None, _shooter=None,
+                 ):
+                 
+        self.tags_Id = _tags_Id
+        self.game_id = _game_id
+        self.addictive = _addictive
+        self.adventure = _adventure
+        self.co_op = _co_op
+        self.comedy = _comedy
+        self.crime = _crime
+        self.drama = _drama
+        self.dystopian_ = _dystopian
+        self.education = _education
+        self.emotional = _emotional
+        self.epic = _epic
+        self.family_friendly = _family_friendly
+        self.farming = _farming
+        self.fighting = _fighting
+        self.flight = _flight
+        self.football = _football
+        self.funny = _funny
+        self.gambling = _gambling
+        self.hacking = _hacking
+        self.horror = _horror
+        self.indie = _indie
+        self.magic = _magic
+        self.mythology = _mythology
+        self.platformer = _platformer
+        self.rpg = _rpg
+        self.shooter = _shooter
